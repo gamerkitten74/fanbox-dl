9fb6fa802b993dd9563937d2d8a1ad198bdebddc
Gamer Kitten <gamerkitten74@gmail.com>
2026-05-14 16:13:45 -0400

    Added more arguments

--- src/cli.rs
+++ src/cli.rs
@@ -1,13 +1,43 @@
-use clap::Parser;
+use clap::{Parser, ValueEnum};
 
-#[derive(Parser)]
+#[derive(Parser, Debug)]
 pub struct Args {
+    /// The Fanbox Creator ID to download from
     #[arg(short, long)]
     pub creator: String,
 
+    /// FANBOXSESSID cookie value for authentication
     #[arg(short, long)]
     pub session_id: String,
 
+    /// The directory to save downloads to
     #[arg(short, long, default_value = "./downloads")]
     pub out_dir: String,
+
+    /// Separates content saved into directories based on the title of the post
+    #[arg(long)]
+    pub dir_by_post: bool,
+
+    /// Ensure all content is downloaded, redownloading existing local content
+    #[arg(long)]
+    pub all: bool,
+
+    /// Skip downloading non-image files from creators
+    #[arg(long)]
+    pub skip_files: bool,
+
+    /// Skip downloading images from creators
+    #[arg(long)]
+    pub skip_images: bool,
+
+    /// Sort downloads by Newest or Oldest
+    #[arg(long, value_enum, default_value_t = Sorting::Newest)]
+    pub sorting: Sorting,
+}
+
+// This enum defines the allowed values for the --sorting argument
+#[derive(ValueEnum, Clone, Debug, PartialEq)]
+pub enum Sorting {
+    Newest,
+    Oldest,
 }
